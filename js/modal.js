d78e4ac1b48c4e789ed73e2677df827d133acdce
Jacob Voyles <jtvkw2@gmail.com>
2018-02-07 15:57:50 -0800

    Fixed content on portfolio page

--- js/modal.js
+++ js/modal.js
@@ -6,7 +6,6 @@ function closeModal(){
   while (modal.firstChild) {
     modal.removeChild(modal.firstChild);
   }
-
 }
 
 window.onclick = function(event) {
@@ -18,6 +17,18 @@ window.onclick = function(event) {
     }
 }
 
+var initialData = {
+  list :[{name: "Memory", type: "Web", url: ko.observable("https://github.com/jtvkw2/Memory"), src: ko.observable("img/Memory.png"), des: "Built a complete browser-based card matching game (also known as Concentration). But this isn’t just any memory game! It’s a shnazzy, well-designed, feature-packed memory game!"},
+  {name:"Bug Boy in Water World", type: "Web", url: ko.observable("https://github.com/jtvkw2/Bug_Boy_in_Water_World"), src: ko.observable("img/Arcade.png"), des: "An HTML5 Canvas powered video game, developed using the best practices in Object Oriented JavaScript."},
+  {name: "Pixel Maker", type:"Web", url: ko.observable("https://github.com/jtvkw2/Pixel-Art"), src: ko.observable("img/Pixel.png"), des: "Created a single-page app in JavaScript where the user can create artistic designs!"},
+  {name: "Feed Reader", type:"Web", url: ko.observable("https://github.com/jtvkw2/Feed-Reader"), src: ko.observable("img/Feed.png"), des: "Wrote comprehensive unit tests, using the Jasmine testing framework, for an RSS Feed Reader application that uses Google's RSS API."},
+  {name: "Google Maps Demo",type:"Web", url: ko.observable("https://github.com/jtvkw2/Map"), src: ko.observable("img/Map.png"), des: "A single-page web application, built using the Knockout framework, that displays a Google Map of an area and various points of interest. Users can search all included landmarks and, when selected, additional information about a landmark is presented from the FourSquare and Wikipedia APIs."},
+  {name:"Pitch Perfect", type: "IOS", url: ko.observable("https://github.com/jtvkw2/Pitch-Perfect"), src: ko.observable("img/Pitch.png"), des: "Built an iPhone app that records a conversation between you and a friend, and then makes your voices sound like a Chipmunk or Darth Vader."},
+  {name:"MeMe", type: "IOS", url: ko.observable("https://github.com/jtvkw2/MeMe"), src: ko.observable("img/MeMe.png"), des: "Built an app that takes pictures and overlays text to make memes out of friends, family, or pets. Also allows sharing with others via social media or email."}
+  ],
+  filters :["All","Web", "IOS"]
+}
+
 function MyViewModel(data) {
    var self = this;
    self.filters = ko.observableArray(data.filters);
@@ -25,7 +36,7 @@ function MyViewModel(data) {
    self.list = ko.observableArray(data.list);
    self.filteredItems = ko.computed(function() {
      var filter = self.filter();
-     if (!filter || filter == "None") {
+     if (!filter || filter == "All") {
       return self.list();
      } else {
       return ko.utils.arrayFilter(self.list(), function(i) {
@@ -36,26 +47,12 @@ function MyViewModel(data) {
 
   self.finalPopup = function(popup) {
     var name = popup["name"];
-    //var index = initialData.list.indexOf(name);
     var index = initialData.list.map(function(e) { return e.name; }).indexOf(name);
-    console.log(index);
     var elem = $("<div class=\'innerModal\' data-bind=\"template: { name: \'modal\',  data: initialData.list["+index+"]}\"></div>");
     $("#myModal").append(elem);
     modal.style.display = "block";
-    //console.log(elem[0]);
     ko.applyBindingsToNode(elem[0]);
   }
-
 };
-
-var initialData = {
-  list :[{name: "memory", type: "JavaScript", url: ko.observable("https://github.com/jtvkw2/Memory"), src: ko.observable("img/Memory.png"), des: "Built a complete browser-based card matching game (also known as Concentration). But this isn’t just any memory game! It’s a shnazzy, well-designed, feature-packed memory game!"},
-  {name:"arcade", type: "JavaScript", url: ko.observable("https://github.com/jtvkw2/Bug_Boy_in_Water_World"), src: ko.observable("img/Arcade.png"), des: "An HTML5 Canvas powered video game, developed using the best practices in Object Oriented JavaScript."},
-  {name: "pixel", type:"JavaScript", url: ko.observable("https://github.com/jtvkw2/Pixel"), src: ko.observable("img/Pixel.png"), des: "Created a single-page app in JavaScript where the user can create artistic designs!"},
-  {name: "feed", type:"JavaScript", url: ko.observable("https://github.com/jtvkw2/Feed-Reader"), src: ko.observable("img/Feed.png"), des: "Wrote comprehensive unit tests, using the Jasmine testing framework, for an RSS Feed Reader application that uses Google's RSS API."},
-  {name: "map",type:"JavaScript", url: ko.observable("https://github.com/jtvkw2/Map"), src: ko.observable("img/Map.png"), des: "A single-page web application, built using the Knockout framework, that displays a Google Map of an area and various points of interest. Users can search all included landmarks and, when selected, additional information about a landmark is presented from the FourSquare and Wikipedia APIs."},
-  {name:"pitch", type: "IOS", url: ko.observable("https://github.com/jtvkw2/Map"), src: ko.observable("img/Map.png"), des: "A Pitch Maniputlation app"}],
-  filters :["None","JavaScript", "IOS"]
-}
 
 ko.applyBindings(new MyViewModel(initialData));
