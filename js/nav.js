9fe0842721af43ed23970943927ead9542733f24
Jacob Voyles <jacobvoyles@Jacobs-MacBook-Pro.local>
2020-04-17 17:19:56 -0500

    Changed viewport css and added new fonts

---
+++ js/nav.js
@@ -0,0 +1,31 @@
+
+//**************** NavBar JS *************************//
+
+
+let menuIcon = document.querySelector('.menuIcon');
+let nav = document.querySelector('.overlay-menu');
+let flex = document.querySelector('.flex-container');
+
+menuIcon.addEventListener('click', () => {
+    if (nav.style.transform != 'translateX(0%)') {
+        nav.style.transform = 'translateX(0%)';
+        flex.style.transform = 'translateY(30%)';
+        nav.style.transition = 'transform 0.2s ease-out';
+    } else {
+        nav.style.transform = 'translateX(-100%)';
+        flex.style.transform = 'translateY(-3%)';
+        nav.style.transition = 'transform 0.2s ease-out';
+    }
+});
+
+
+
+let toggleIcon = document.querySelector('.menuIcon');
+
+toggleIcon.addEventListener('click', () => {
+    if (toggleIcon.className != 'menuIcon toggle') {
+        toggleIcon.className += ' toggle';
+    } else {
+        toggleIcon.className = 'menuIcon';
+    }
+});
