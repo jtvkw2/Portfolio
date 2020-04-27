12bf3164664f2e29ea045ec4acb1d4bdd0ea0beb
Jacob Voyles <jacobvoyles@Jacobs-MacBook-Pro.local>
2020-04-27 13:10:55 -0500

    Fixed Index Nav and made Portfolio page listing dynamic from array

--- js/main.js
+++ js/main.js
@@ -41,3 +41,50 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+// **************** Portfolio DIVS ****************//
+document.body.onload = createDivs;
+
+function createDivs(){
+  var portfolioArray = [
+    ["html","img/Bugboy.png","Bug Boy in Water World", "https://github.com/jtvkw2/Bug_Boy_in_Water_World"],
+    ["mobile","img/Pitch.jpg","Pitch Perfect","https://github.com/jtvkw2/Pitch-Perfect"]
+  ];
+  for(var i=0; i< portfolioArray.length; i++){
+    addElement(portfolioArray[i][0],portfolioArray[i][1],portfolioArray[i][2],portfolioArray[i][3]);
+    //console.log(addElement(portfolioArray[i][0],portfolioArray[i][1],portfolioArray[i][2],portfolioArray[i][3]));
+  }
+};
+
+function addElement (w,x,y,z) {
+  var div1 = document.createElement("div");
+  div1.classList.add("column",w, "show");
+  var div2 = document.createElement("div");
+  div1.appendChild(div2);
+  div2.classList.add("content");
+  var div3 = document.createElement("div");
+  div2.appendChild(div3);
+  div2.classList.add("grid");
+  var figure = document.createElement("figure");
+  div3.appendChild(figure);
+  div2.classList.add("effect-apollo","port-img");
+  var img = document.createElement("img");
+  figure.appendChild(img);
+  img.classList.add("imgStyle");
+  img.src = x;
+  var figcap = document.createElement("figcaption");
+  figure.appendChild(figcap);
+  var h2 = document.createElement("h2");
+  figcap.appendChild(h2);
+  var newContent = document.createTextNode(y);
+  h2.appendChild(newContent);
+  var p = document.createElement("p");
+  var github = document.createTextNode("Link To Github");
+  figcap.appendChild(p);
+  var a = document.createElement("a");
+  a.href = z;
+  p.appendChild(a);
+  a.appendChild(github);
+  var currentDiv = document.getElementById("currentDiv");
+  currentDiv.append(div1);
+}
